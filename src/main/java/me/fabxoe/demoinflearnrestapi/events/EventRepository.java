96a3fa4a7bba5246bf6a59a431465e7a3cf92eea
Sungmin Oh <fabxoe.kor@gmail.com>
2020-01-16 22:17:46 +0900

    POST /api/events returns 201

---
+++ src/main/java/me/fabxoe/demoinflearnrestapi/events/EventRepository.java
@@ -0,0 +1,7 @@
+package me.fabxoe.demoinflearnrestapi.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+
+}
